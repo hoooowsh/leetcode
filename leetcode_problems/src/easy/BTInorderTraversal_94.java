185ff21829a24b9305f51fcd496d1e791fa5a341
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-05-04 22:10:21 -0400

    added 94

--- leetcode_problems/src/easy/BTInorderTraversal_94.java
+++ leetcode_problems/src/easy/BTInorderTraversal_94.java
@@ -1,5 +1,43 @@
 package easy;
 
-public class BTInorderTraversal_94 {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BTInorderTraversal_94 {
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+	public static List<Integer> inorderTraversal(TreeNode root) {
+		ArrayList<Integer> list = new ArrayList<>();
+		helper(root, list);
+		return list;
+	}
+
+	public static void helper(TreeNode root, ArrayList<Integer> list) {
+		if (root == null) {
+			return;
+		}
+		helper(root.left, list);
+		helper(root.right, list);
+	}
 }
