7fd5b3a0997e254f03d617529fde03f5019841da
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-05-09 23:05:58 -0400

    pushed 15 with no code

---
+++ leetcode_problems/src/medium/ThreeSum_15.java
@@ -0,0 +1,20 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ThreeSum_15 {
+	public static void main(String[] args) {
+	}
+
+	public List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>> arr = new ArrayList<List<Integer>>();
+		if (nums.length < 3) {
+			return arr;
+		}
+		for (int i = 0; i < nums.length; i++) {
+
+		}
+		return arr;
+	}
+}
