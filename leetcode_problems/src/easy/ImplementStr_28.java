4c09316fbf9a48eb90bf32ab33e3cce4ef578565
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-05-11 22:28:15 -0400

    added 31

--- leetcode_problems/src/easy/ImplementStr_28.java
+++ leetcode_problems/src/easy/ImplementStr_28.java
@@ -1,6 +1,9 @@
 package easy;
 
 public class ImplementStr_28 {
+	public static void main(String[] args) {
+
+	}
 	public int strStr(String haystack, String needle) {
         if (haystack.contains(needle)) {
 			return haystack.indexOf(needle);
---
+++ leetcode_problems/src/medium/NextPermutation_31.java
@@ -0,0 +1,11 @@
+package medium;
+
+public class NextPermutation_31 {
+	public static void main(String[] args) {
+
+	}
+
+	public void nextPermutation(int[] nums) {
+
+	}
+}
