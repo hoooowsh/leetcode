09c9bb3d48ed0b64bc4fb16cb04e09781e82f0d7
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-07-24 12:19:19 -0400

    added 108 start

---
+++ leetcode_problems/src/easy/ConvertSortedArraytoBST_108.java
@@ -0,0 +1,28 @@
+package easy;
+
+public class ConvertSortedArraytoBST_108 {
+	public static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode() {
+		}
+
+		TreeNode(int val) {
+			this.val = val;
+		}
+
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public static TreeNode sortedArrayToBST(int[] nums) {
+		TreeNode root = new TreeNode();
+		return root;
+	}
+
+}
