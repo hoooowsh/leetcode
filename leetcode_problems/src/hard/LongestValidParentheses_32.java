4a83b85d4464b8e4bf0aa1a2c9427bd3bedc756f
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-08-12 16:14:09 -0400

    added 32

---
+++ leetcode_problems/src/hard/LongestValidParentheses_32.java
@@ -0,0 +1,42 @@
+package hard;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class LongestValidParentheses_32 {
+	public static void main(String[] args) {
+		String test = "(()";
+		int len = longestValidParentheses(test);
+		System.out.println(len);
+	}
+
+	public static int longestValidParentheses(String s) {
+		Stack<Integer> stack = new Stack<Integer>();
+		boolean[] arr = new boolean[s.length()];
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '(') {
+				stack.add(i);
+			} else {
+				if (!stack.isEmpty()) {
+					int start = stack.pop();
+					arr[start] = true;
+					arr[i] = true;
+				}
+			}
+		}
+		int max = 0;
+		int counter = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == true) {
+				counter++;
+				if (counter > max) {
+					max = counter;
+				}
+			} else {
+				counter = 0;
+			}
+		}
+
+		return max;
+	}
+}
