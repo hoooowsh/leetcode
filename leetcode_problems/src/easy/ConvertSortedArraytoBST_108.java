a3024f7b6e431beda575a3b8555fa64d2dc59f41
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-08-12 12:23:14 -0400

    added 108

--- leetcode_problems/src/easy/ConvertSortedArraytoBST_108.java
+++ leetcode_problems/src/easy/ConvertSortedArraytoBST_108.java
@@ -20,9 +20,35 @@ public class ConvertSortedArraytoBST_108 {
 		}
 	}
 
+	public static void main(String[] args) {
+		int[] arr = { -10, -3, 0, 5, 9 };
+		TreeNode test = sortedArrayToBST(arr);
+		System.out.println(test.val);
+	}
+
+	// using BST kind of thing
 	public static TreeNode sortedArrayToBST(int[] nums) {
 		TreeNode root = new TreeNode();
+		int mid = (nums.length - 1) / 2;
+		root.val = nums[mid];
+		helper(0, mid - 1, nums, root, true);
+		helper(mid + 1, nums.length - 1, nums, root, false);
 		return root;
 	}
 
+	public static void helper(int start, int end, int[] nums, TreeNode current, boolean left) {
+		if (start > end) {
+			return;
+		}
+		int mid = start + (end - start) / 2;
+		TreeNode temp = new TreeNode(nums[mid]);
+		if (left) {
+			current.left = temp;
+		} else {
+			current.right = temp;
+		}
+		helper(start, mid - 1, nums, temp, true);
+		helper(mid + 1, end, nums, temp, false);
+
+	}
 }
