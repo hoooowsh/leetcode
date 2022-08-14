eb72178d692591c66667f058482737ac5fbe6e34
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-08-14 10:11:09 -0400

    added 141

--- leetcode_problems/src/easy/LinkedListCycle_141.java
+++ leetcode_problems/src/easy/LinkedListCycle_141.java
@@ -6,16 +6,37 @@ public class LinkedListCycle_141 {
 	public static void main(String[] args) {
 	}
 
+	// easy approach but a bit slow, using hashset to record if there is a duplicate
 	public static boolean hasCycle(ListNode head) {
 		HashSet<ListNode> hash = new HashSet<ListNode>();
 		while (head != null) {
-			if(hash.contains(head)) {
+			if (hash.contains(head)) {
 				return true;
 			}
 			hash.add(head);
 			head = head.next;
 		}
 		return false;
+	}
+
+	// smart approach using two pointer temp node to record, fast and slow, fast
+	// moves 2 at a time and slow moves 1 at a time, fast will eventually reach slow
+	// if there is a cycle
+	public static boolean hasCycle2(ListNode head) {
+		if (head == null) {
+			return false;
+		}
+
+		ListNode slow = head;
+		ListNode fast = head.next;
+		while (slow != fast) {
+			if (fast == null || fast.next == null) {
+				return false;
+			}
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return true;
 	}
 
 	class ListNode {
