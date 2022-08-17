63c47f8dfef17524d02d51a87366223ccd47768b
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-08-16 21:58:33 -0400

    added 50

---
+++ leetcode_problems/src/medium/Powxn_50.java
@@ -0,0 +1,11 @@
+package medium;
+
+public class Powxn_50 {
+	public static void main(String[] args) {
+	}
+
+	// very smart version, I'm a genius
+	public static double myPow(double x, int n) {
+		return Math.pow(x, n);
+	}
+}
