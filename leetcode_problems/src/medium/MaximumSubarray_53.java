7d5eb3ef50507031dd113e9cf0a9c64102277e06
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-08-18 12:13:27 -0400

    added 53

--- leetcode_problems/src/medium/MaximumSubarray_53.java
+++ leetcode_problems/src/medium/MaximumSubarray_53.java
@@ -2,9 +2,45 @@ package medium;
 
 public class MaximumSubarray_53 {
 	public static void main(String[] args) {
+		int[] test = { -2, -1 };
+		int tt = maxSubArray(test);
+		System.out.println(tt);
 	}
 
-	public int maxSubArray(int[] nums) {
-		return 0;
+	// count for current max and then record it to another array, kind of DP
+	public static int maxSubArray(int[] nums) {
+		if (nums == null) {
+			return 0;
+		}
+		if (nums.length == 1) {
+			return nums[0];
+		}
+		int result = nums[0];
+		// this array records the max val for current
+		int[] max = new int[nums.length];
+		max[0] = nums[0];
+		for (int i = 1; i < nums.length; i++) {
+			if (max[i - 1] < 0) {
+				max[i] = nums[i];
+				if (max[i] > result) {
+					result = max[i];
+				}
+				continue;
+			}
+			int cur = max[i - 1] + nums[i];
+			if (cur > 0) {
+				max[i] = cur;
+				if (max[i] > result) {
+					result = max[i];
+				}
+			} else {
+				max[i] = nums[i];
+				if (max[i] > result) {
+					result = max[i];
+				}
+			}
+		}
+		return result;
 	}
+
 }
