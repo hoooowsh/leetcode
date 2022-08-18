aadb6e2a94462b6351d8228d1e2c2111c52bcf2d
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-08-18 08:19:09 -0400

    added 206

---
+++ leetcode_problems/src/easy/ReverseLinkedList_206.java
@@ -0,0 +1,37 @@
+package easy;
+
+public class ReverseLinkedList_206 {
+	public static void main(String[] args) {
+	}
+
+	public ListNode reverseList(ListNode head) {
+		ListNode currnode = head.next;
+		ListNode prevnode = head;
+		head.next = null;
+		while (currnode != null) {
+			ListNode temp = currnode.next;
+			currnode.next = prevnode;
+			prevnode = currnode;
+			currnode = temp;
+		}
+		return prevnode;
+	}
+
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+}
---
+++ leetcode_problems/src/medium/MaximumSubarray_53.java
@@ -0,0 +1,10 @@
+package medium;
+
+public class MaximumSubarray_53 {
+	public static void main(String[] args) {
+	}
+
+	public int maxSubArray(int[] nums) {
+		return 0;
+	}
+}
