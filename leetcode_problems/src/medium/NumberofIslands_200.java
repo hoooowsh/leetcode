5ceb38f03bfe9c63e4c5a9277f984afc13129559
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-09-15 10:54:31 -0400

    200 left over

---
+++ leetcode_problems/src/medium/NumberofIslands_200.java
@@ -0,0 +1,18 @@
+package medium;
+
+public class NumberofIslands_200 {
+
+	public static void main(String args[]) {
+
+	}
+
+	public static int numIslands(char[][] grid) {
+		return 1;
+	}
+
+	public static void helper(char[][] grid, int row, int col) {
+		while()
+		for(int j = col)
+		}
+	}
+}
