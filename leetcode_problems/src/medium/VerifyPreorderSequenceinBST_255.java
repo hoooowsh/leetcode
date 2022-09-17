997c5ffbc916e0063cf240c08d239707e78d70d0
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-09-16 22:31:52 -0400

    added 255 not finished

---
+++ leetcode_problems/src/medium/VerifyPreorderSequenceinBST_255.java
@@ -0,0 +1,12 @@
+package medium;
+
+public class VerifyPreorderSequenceinBST_255 {
+	public static void main(String args[]) {
+	}
+
+	// slow version, build the tree first then check if the tree is BST
+	public static boolean verifyPreorder(int[] preorder) {
+		
+		return false;
+	}
+}
