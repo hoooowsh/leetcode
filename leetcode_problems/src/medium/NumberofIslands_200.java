d5a86910d98c3028b0c1257b25d4e1bbf78b91e9
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-09-17 15:52:02 -0400

    added 200

--- leetcode_problems/src/medium/NumberofIslands_200.java
+++ leetcode_problems/src/medium/NumberofIslands_200.java
@@ -3,16 +3,40 @@ package medium;
 public class NumberofIslands_200 {
 
 	public static void main(String args[]) {
-
+		char[][] grid = { { '1', '1', '1', '1', '0' }, { '1', '1', '0', '1', '0' }, { '1', '1', '0', '0', '0' },
+				{ '0', '0', '0', '0', '0' } };
+		int i = numIslands(grid);
+		System.out.println(i);
 	}
 
+	// dfs version, using recursion. should not be that slow tho, leetcode similar
+	// dfs version beats 90
 	public static int numIslands(char[][] grid) {
-		return 1;
+		int counter = 2;
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == '1') {
+					helper(grid, i, j, (char) (counter + '0'));
+					counter++;
+				}
+			}
+		}
+		return counter - 2;
 	}
 
-	public static void helper(char[][] grid, int row, int col) {
-//		while()
-//		for(int j = col)
-//		}
+	public static void helper(char[][] grid, int row, int col, char num) {
+		if (row >= grid.length || col >= grid[0].length || col < 0 || row < 0) {
+			return;
+		}
+		if (grid[row][col] == '1') {
+			grid[row][col] = num;
+			helper(grid, row + 1, col, num);
+			helper(grid, row - 1, col, num);
+			helper(grid, row, col + 1, num);
+			helper(grid, row, col - 1, num);
+		} else {
+			return;
+		}
 	}
+
 }
