06c893f22883a297e00d9ea2ecb87491c4fd4b89
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-09-22 16:50:55 -0400

    added 818 not finish

---
+++ leetcode_problems/src/hard/RaceCar_818.java
@@ -0,0 +1,12 @@
+package hard;
+
+public class RaceCar_818 {
+	public static void main(String args[]) {
+
+	}
+
+	// pos 0, 1, 3, 7, 15, 31... will remain the shortest
+	public static int racecar(int target) {
+		return 0;
+	}
+}
