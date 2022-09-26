38b66e31568876d65a5b7f41a3ba22c0569f9df3
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-09-25 20:10:26 -0400

    added 2115

---
+++ leetcode_problems/src/medium/FindAllPossibleRecipesfromGivenSupplies_2115.java
@@ -0,0 +1,15 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllPossibleRecipesfromGivenSupplies_2115 {
+	public static void main(String args[]) {
+	}
+
+	public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+		List<String> result = new ArrayList<>();
+		
+		return result;
+	}
+}
--- leetcode_problems/src/medium/LongestStringChain_1048.java
+++ leetcode_problems/src/medium/LongestStringChain_1048.java
@@ -1,6 +1,8 @@
 package medium;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class LongestStringChain_1048 {
 	public static void main(String args[]) {
@@ -10,6 +12,32 @@ public class LongestStringChain_1048 {
 	}
 
 	public static int longestStrChain(String[] words) {
+		// sort array by string length
+		Arrays.sort(words, (a, b) -> Integer.compare(a.length(), b.length()));
+		HashMap<Integer, HashSet<String>> map = new HashMap<>();
+		for (int i = 0; i < words.length; i++) {
+			// add one length string anyway
+			if (words[i].length() == words[0].length()) {
+				if (i == 0) {
+					HashSet<String> set = new HashSet<>();
+					set.add(words[i]);
+					map.put(1, set);
+					continue;
+				} else {
+					map.get(1).add(words[i]);
+					continue;
+				}
+			}
+			// if the length already exist
+			if (map.keySet().contains(words[i].length())) {
+
+			}
+			// if the length not exist yet
+			else {
+				HashSet<String> set = new HashSet<>();
+//				set.add(word);
+			}
+		}
 		return 0;
 	}
 }
