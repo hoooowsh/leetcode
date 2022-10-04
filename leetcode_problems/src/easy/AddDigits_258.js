3ba4d5037d8fc3761a4aafdd2b37952aabae0123
Sheng <83986522+hoooowsh@users.noreply.github.com>
2022-10-04 09:42:41 -0400

    modified 258

--- leetcode_problems/src/easy/AddDigits_258.js
+++ leetcode_problems/src/easy/AddDigits_258.js
@@ -3,5 +3,14 @@
  * @return {number}
  */
  var addDigits = function(num) {
-    
+    var temp = 0;
+    while(num >= 10){
+        while(num != 0){
+            temp += num % 10;
+            num = Math.floor(num / 10);
+        }
+        num = temp;
+        temp = 0;
+    }
+    return num;
 };
