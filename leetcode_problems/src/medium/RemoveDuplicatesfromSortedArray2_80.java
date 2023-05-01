677cef6b0162fd1506ad3f1363cba24f7ad0fd19
Sheng <83986522+hoooowsh@users.noreply.github.com>
2023-05-01 17:41:45 -0400

    added 80

--- leetcode_problems/src/medium/RemoveDuplicatesfromSortedArray2_80.java
+++ leetcode_problems/src/medium/RemoveDuplicatesfromSortedArray2_80.java
@@ -6,6 +6,31 @@ public class RemoveDuplicatesfromSortedArray2_80 {
 	}
 
 	public int removeDuplicates(int[] nums) {
-		return 0;
+		int count = 0;
+		int current = nums[0];
+		int pointer = 0;
+		for (int i = 0; i < nums.length; i++) {
+			// if current same as previous
+			if (nums[i] == current) {
+				count++;
+				// if the current should be remember
+				if (count <= 2) {
+					nums[pointer] = current;
+					pointer++;
+				}
+				// if the current should not be remember
+				else {
+					continue;
+				}
+			}
+			// if current not same as previous
+			else {
+				count = 1;
+				current = nums[i];
+				nums[pointer] = current;
+				pointer++;
+			}
+		}
+		return pointer;
 	}
 }
