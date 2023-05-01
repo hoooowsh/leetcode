d9436006c4e187e8d271e633d2bb975f4bc6e16c
Sheng <83986522+hoooowsh@users.noreply.github.com>
2023-04-30 21:57:57 -0400

    added 374

---
+++ leetcode_problems/src/easy/GuessNumberHigherroLower_374.java
@@ -0,0 +1,31 @@
+package easy;
+
+public class GuessNumberHigherroLower_374 {
+//	public class Solution extends GuessGame {
+//		public int guessNumber(int n) {
+//			if (guess(1) == 0) {
+//				return 1;
+//			}
+//			if (guess(n) == 0) {
+//				return n;
+//			}
+//			int lo = 1;
+//			int hi = n;
+//			int mid = 0;
+//			while (lo <= hi) {
+//				mid = lo + (hi - lo) / 2;
+//
+//				int result = guess(mid);
+//				if (result == 0) {
+//					break;
+//				}
+//				if (result == 1) {
+//					lo = mid + 1;
+//				} else {
+//					hi = mid - 1;
+//				}
+//			}
+//			return mid;
+//		}
+//	}
+}
