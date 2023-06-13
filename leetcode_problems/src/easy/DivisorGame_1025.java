9467b59eb69c705cd0cef3e5c91e422bed512fdf
Sheng <83986522+hoooowsh@users.noreply.github.com>
2023-06-12 22:59:42 -0400

    added 1025

---
+++ leetcode_problems/src/easy/DivisorGame_1025.java
@@ -0,0 +1,14 @@
+package easy;
+
+public class DivisorGame_1025 {
+	public static void main(String[] args) {
+
+	}
+
+	public static boolean divisorGame(int n) {
+		if (n % 2 == 0) {
+			return true;
+		}
+		return false;
+	}
+}
