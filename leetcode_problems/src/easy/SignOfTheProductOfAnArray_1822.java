44898dc0f713f0f96b7b415257fc936bdbe1ad3b
Sheng <83986522+hoooowsh@users.noreply.github.com>
2023-10-30 20:49:11 -0400

    added 1822

---
+++ leetcode_problems/src/easy/SignOfTheProductOfAnArray_1822.java
@@ -0,0 +1,23 @@
+package easy;
+
+public class SignOfTheProductOfAnArray_1822 {
+	public static void main(String[] args) {
+
+	}
+
+	public int arraySign(int[] nums) {
+		int neg = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 0) {
+				return 0;
+			}
+			if (nums[i] < 0) {
+				neg++;
+			}
+		}
+		if (neg % 2 != 0) {
+			return -1;
+		}
+		return 1;
+	}
+}
