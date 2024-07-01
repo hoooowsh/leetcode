2fe129a295e89825f04928244dba023d31c7bb32
Shawn <haosheng950403@gmail.com>
2024-07-01 15:04:27 -0400

    test

---
+++ leetcode_problems/src/medium/PathSum2_113.java
@@ -0,0 +1,29 @@
+package medium;
+
+import java.util.List;
+
+public class PathSum2_113 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        
+    }
+}
