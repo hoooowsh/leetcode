98f27768df3bd1879843e8ca5c20c26786764241
hoooowsh <haosheng950403@gmail.com>
2024-07-22 15:44:38 -0400

    added 2418

---
+++ leetcode_problems/src/easy/SortThePeople_2418.java
@@ -0,0 +1,24 @@
+package easy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+
+public class SortThePeople_2418 {
+    public static void main(String[] args) {
+    }
+
+    public static String[] sortPeople(String[] names, int[] heights) {
+        HashMap<Integer, String> map = new HashMap<>();
+        for (int i = 0; i < heights.length; i++) {
+            map.put(heights[i], names[i]);
+        }
+
+        Arrays.sort(heights);
+        String[] result = new String[heights.length];
+        for (int i = heights.length - 1; i >= 0; i--) {
+            result[result.length - i - 1] = map.get(heights[i]);
+        }
+        return result;
+    }
+}
